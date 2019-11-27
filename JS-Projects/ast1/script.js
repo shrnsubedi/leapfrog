4abc4284d4cee756317d811a804783066460cc30
shrnsubedi <shrnsubedi22@gmail.com>
2019-11-27 08:02:52 +0545

    Added button and indicators

--- JS-Projects/ast1/script.js
+++ JS-Projects/ast1/script.js
@@ -4,26 +4,69 @@ var index = 0;
 var widthImage = 400;
 var moveImagePixel = 400;
 var heightImage = 200;
+var direction = "right";
 
 var carouselContainer = document.getElementById('carousel-container');
 var carouselImageWrapper = document.getElementById('carousel-image-wrapper');
-console.log(carouselContainer);
-console.log(carouselImageWrapper);
-console.log(imageCount);
 
 carouselContainer.style.width = widthImage + 'px';
 carouselContainer.style.height = heightImage + 'px';
 
 carouselImageWrapper.style.width = imageCount * widthImage;
 
-setInterval(function () {
-  if (index == imageCount) {
-    index = 0;
-    clearInterval();
+var dots = document.getElementsByTagName("span")
+console.log(dots);
+/* Code to implement image switch with dot press
+function dotOnclick(clickedId) {
+  console.log(clickedId);
+  switch (clickedId) {
+    case 1:
+      index = 0;
+      break;
+    case 2:
+      index = 1;
+      break;
+    case 3:
+      index = 2;
+      break;
+    case 4:
+      index = 3;
   }
+}
+*/
+function setDirectionLeft() {
+  direction = "left";
+}
+
+function setDirectionRight() {
+  direction = "right";
+}
+
+setInterval(function () {
+  if (direction == "left") {
+    if (index == imageCount) {
+      index = 0;
+    }
+    else {
+      carouselImageWrapper.style.left = index * -moveImagePixel + 'px';
+      carouselImageWrapper.style.transition = "all 2s";
+      dots[index].setAttribute("class", "inactive");
+      index++;
+      dots[index].setAttribute("class", "active");
+    }
+  }
+
   else {
-    carouselImageWrapper.style.left = index * -moveImagePixel + 'px';
-    carouselImageWrapper.style.transition = "all 2s";
-    index++;
+    if (index == imageCount) {
+      index = 0;
+    }
+    else {
+      carouselImageWrapper.style.right = index * -moveImagePixel + 'px';
+      carouselImageWrapper.style.left = "";
+      carouselImageWrapper.style.transition = "all 2s";
+      dots[index].setAttribute("class", "inactive");
+      index++;
+      dots[index].setAttribute("class", "active");
+    }
   }
 }, 2000);

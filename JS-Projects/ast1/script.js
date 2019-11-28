d92a3526e3d59838f2b52dc3cb43fb909c48dd15
shrnsubedi <shrnsubedi22@gmail.com>
2019-11-28 08:45:37 +0545

    Added assignment 2 with changes to assignment 1

--- JS-Projects/ast1/script.js
+++ JS-Projects/ast1/script.js
@@ -1,10 +1,10 @@
 var imgArray = document.images;
 var imageCount = imgArray.length;
-var index = 0;
+var index;
 var widthImage = 400;
 var moveImagePixel = 400; //Used for JS only transition effect
 var heightImage = 200;
-var direction = 'right';
+var id;
 
 var carouselContainer = document.getElementById('carousel-container');
 var carouselImageWrapper = document.getElementById('carousel-image-wrapper');
@@ -14,59 +14,59 @@ carouselContainer.style.height = heightImage + 'px';
 
 carouselImageWrapper.style.width = imageCount * widthImage;
 
-var dots = document.getElementsByTagName('span')
+var dots = document.getElementsByTagName('span');
 
-/* Code to implement image switch with dot press(Not functional)
+//Code to implement image switch with dot press
 function dotOnclick(clickedId) {
   console.log(clickedId);
-  switch (clickedId) {
+  idInt = parseInt(clickedId);
+  switch (idInt) {
+    case 0:
+      clearInterval(id);
+      changeImage(idInt);
+      break;
     case 1:
-      index = 0;
+      clearInterval(id);
+      changeImage(idInt);
       break;
     case 2:
-      index = 1;
+      clearInterval(id);
+      changeImage(idInt);
       break;
     case 3:
-      index = 2;
-      break;
-    case 4:
-      index = 3;
+      clearInterval(id);
+      changeImage(idInt);
   }
 }
-*/
-function setDirectionLeft() {
-  direction = 'left';
+
+function buttonPrev() {
+  clearInterval(id);
+  carouselImageWrapper.style.left = 1 * -moveImagePixel + 'px';
+  changeImage(index - 1);
 }
 
-function setDirectionRight() {
-  direction = 'right';
+function buttonNext() {
+  clearInterval(id);
+  carouselImageWrapper.style.right = 1 * moveImagePixel + 'px';
+  changeImage(index);
 }
 
-setInterval(function () {
-  if (direction == 'left') {
+function changeImage(imgIndex) {
+  index = imgIndex;
+  id = setInterval(function () {
     if (index == imageCount) {
       index = 0;
     }
     else {
       carouselImageWrapper.style.left = index * -moveImagePixel + 'px';
-      carouselImageWrapper.style.transition = 'all 2s';
-      dots[index].setAttribute('class', 'inactive');
-      index++;
+      carouselImageWrapper.style.transition = 'all 0.5s';
+      for (i = 0; i < imageCount; i++) {
+        dots[i].setAttribute('class', 'inactive');
+      }
       dots[index].setAttribute('class', 'active');
+      index++;
     }
-  }
+  }, 1000);
+}
 
-  else {
-    if (index == imageCount) {
-      index = 0;
-    }
-    else {
-      carouselImageWrapper.style.right = index * -moveImagePixel + 'px';
-      carouselImageWrapper.style.left = "";
-      carouselImageWrapper.style.transition = 'all 2s';
-      dots[index].setAttribute('class', 'inactive');
-      index++;
-      dots[index].setAttribute('class', 'active');
-    }
-  }
-}, 2000);
+changeImage(index);
---
+++ JS-Projects/ast2/script.js
@@ -0,0 +1,97 @@
+function Carousel(carouselContainer, carouselImageWrapper, widthImage, heightImage, delay, transitionTime, imageCount) {
+  this.carouselContainer = carouselContainer;
+  this.carouselImageWrapper = carouselImageWrapper;
+  this.dotArray = [];
+  this.widthImage = widthImage;
+  this.heightImage = heightImage;
+  this.imageCount = imageCount;
+  this.id;
+  this.moveImagePixel = -400;
+  this.index = 0;
+  this.delay = delay;
+  this.transitionTime = transitionTime;
+
+  this.init = function () {
+    this.carouselContainer.style.width = this.widthImage + 'px';
+    this.carouselContainer.style.height = this.heightImage + 'px';
+    this.carouselImageWrapper.style.width = this.imageCount * this.widthImage;
+  }
+
+  this.generateDots = function () {
+    var dotVar = document.createElement('div');
+    dotVar.setAttribute('class', 'dot-buttons');
+    dotVar.style.textAlign = 'center';
+    for (var j = 0; j < imageCount; j++) {
+      this.dotArray[j] = document.createElement("span");
+    }
+  }
+
+  this.debugFunction = function () {
+    console.log(this.carouselContainer);
+    console.log(this.carouselImageWrapper);
+    console.log(this.dots);
+    console.log(this.widthImage);
+    console.log(this.heightImage);
+    console.log(this.imageCount);
+    console.log(this.moveImagePixel);
+    console.log(this.index);
+    console.log(this.delay);
+  }
+
+  this.changeImage = function (imgIndex) {
+    this.index = imgIndex
+    if (this.index == this.imageCount) {
+      this.index = 0;
+    }
+    else {
+      this.carouselImageWrapper.style.left = this.index * this.moveImagePixel + 'px';
+      this.carouselImageWrapper.style.transition = this.delay;
+      this.carouselImageWrapper.style.transition.delay = this.delay;
+      for (var i = 0; i < this.imageCount; i++) {
+        this.dotsArray[i].setAttribute('class', 'inactive');
+      }
+      this.dotsArray[index].setAttribute('class', 'active');
+      this.index = this.index + 1;
+    }
+  }
+
+  this.dotOnclick = function (clickedId) {
+    idInt = parseInt(clickedId);
+    switch (idInt) {
+      case 0:
+        clearInterval(id);
+        intervalFunction(idInt);
+        break;
+      case 1:
+        clearInterval(id);
+        intervalFunction(idInt);
+        break;
+      case 2:
+        clearInterval(id);
+        intervalFunction(idInt);
+        break;
+      case 3:
+        clearInterval(id);
+        intervalFunction(idInt);
+    }
+  }
+
+  this.intervalFunction = function (dotIndex) {
+    this.id = setInterval(this.changeImage(dotIndex), this.transitionTime);
+  }
+
+}
+
+var carouselContainerOut = document.getElementById('carousel-container');
+var carouselImageWrapperOut = document.getElementById('carousel-image-wrapper');
+var widthImage = 400;
+var heightImage = 200;
+var transitionTime = 1000;
+var delay = "1s";
+var imgArray = document.images;
+imageCount = imgArray.length;
+
+var newCarousel = new Carousel(carouselContainerOut, carouselImageWrapperOut, widthImage, heightImage, delay, transitionTime, imageCount)
+newCarousel.init();
+newCarousel.intervalFunction();
+newCarousel.debugFunction();
